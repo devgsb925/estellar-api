e81257b4880cd4cf152a771f04ad8c59e1476c7b
Yaj Bolisay <gsbolisay925@gmail.com>
2023-12-26 14:09:38 +0700

    feat: add api pipelines

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("hellow", "world");
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import compression from 'compression';
+
+const app = express();
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    credentials: true
+}));                
+
+
+app.get('/test', (req: express.Request, res: express.Response)=>{
+    console.log('hello world');
+    
+    res.send('Hello World');
+    });
+
+
+const server = http.createServer(app);
+server.listen(8080, () => console.log(`app listening on port http://localhost:8080`));
